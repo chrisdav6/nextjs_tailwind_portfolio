7a5d4a8f19b570a1ec8c0a7e764bca7d8a13c9bf
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-10-04 14:41:55 -0400

    Incorporated scroll snap

---
+++ components/Portfolio.js
@@ -0,0 +1,21 @@
+const Portfolio = () => {
+  return (
+    <div className='max-w-5xl mx-auto md:text-center'>
+      <h3 className='text-3xl py-1'>Portfolio</h3>
+      <p className='text-md py-2 leading-8 text-gray-800'>
+        Since the beginning of my journey as a freelance designer and devloper,
+        I've done remote work for{' '}
+        <span className='text-teal-500'> agencies </span> consulted for{' '}
+        <span className='text-teal-500'> startups </span> and collaborated with
+        talented people to create digital products for both business and
+        consumer use.
+      </p>
+      <p className='text-md py-2 leading-8 text-gray-800'>
+        I offer a wide range of services, including brand design, programming
+        and teaching.
+      </p>
+    </div>
+  );
+};
+
+export default Portfolio;
