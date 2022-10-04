e1f84e6fffd0f12a827c38afdc752e665814fac9
chris.davis5440@gmail.com <chris.davis5440@gmail.com>
2022-10-04 14:18:15 -0400

    Completed added content to card container

--- components/CardContainer.js
+++ components/CardContainer.js
@@ -5,11 +5,11 @@ import consulting from '../public/img/consulting.png';
 
 const CardContainer = () => {
   return (
-    <div className='flex flex-col md:flex-row max-w-7xl gap-x-5 mx-auto mt-10'>
+    <div className='flex flex-col md:flex-row max-w-7xl gap-x-5 mx-auto justify-center mt-10'>
       <div className='text-center shadow-lg p-10 rounded-xl my-10'>
         <Image src={design} width={100} height={100} />
         <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-        <p className='py-2'>
+        <p className='py-2 max-w-[300px]'>
           Creating elegant designs suited for your needs following core design
           theory
         </p>
@@ -21,28 +21,26 @@ const CardContainer = () => {
 
       <div className='text-center shadow-lg p-10 rounded-xl my-10'>
         <Image src={code} width={100} height={100} />
-        <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-        <p className='py-2'>
-          Creating elegant designs suited for your needs following core design
-          theory
+        <h3 className='text-lg font-medium pt-8 pb-2'>Precise Code</h3>
+        <p className='py-2 max-w-[300px]'>
+          Building strong front and back ends using industry standard tactics
         </p>
-        <h4 className='py-4 text-teal-600'>Design tools I use</h4>
-        <p className='text-gray-800 py-1'>Photoshop</p>
-        <p className='text-gray-800 py-1'>Illustrator</p>
-        <p className='text-gray-800 py-1'>Figma</p>
+        <h4 className='py-4 text-teal-600'>Programming tools I use</h4>
+        <p className='text-gray-800 py-1'>VS Code</p>
+        <p className='text-gray-800 py-1'>GitHub</p>
+        <p className='text-gray-800 py-1'>Next.js</p>
       </div>
 
       <div className='text-center shadow-lg p-10 rounded-xl my-10'>
         <Image src={consulting} width={100} height={100} />
-        <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
-        <p className='py-2'>
-          Creating elegant designs suited for your needs following core design
-          theory
+        <h3 className='text-lg font-medium pt-8 pb-2'>Expert Consultation</h3>
+        <p className='py-2 max-w-[300px]'>
+          Working with you to bring your greatest ideas to life.
         </p>
-        <h4 className='py-4 text-teal-600'>Design tools I use</h4>
-        <p className='text-gray-800 py-1'>Photoshop</p>
-        <p className='text-gray-800 py-1'>Illustrator</p>
-        <p className='text-gray-800 py-1'>Figma</p>
+        <h4 className='py-4 text-teal-600'>Consultation tools I use</h4>
+        <p className='text-gray-800 py-1'>Communication</p>
+        <p className='text-gray-800 py-1'>Scheduling</p>
+        <p className='text-gray-800 py-1'>Deadlines</p>
       </div>
     </div>
   );
